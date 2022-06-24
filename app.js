b213bf7acf01d107ad25bbcaa8976a29d373600d
tenconi <47565305+tenconi@users.noreply.github.com>
2022-06-24 13:49:03 +0200

    :skull: aesthetic adjust

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 let show = document.getElementById("show");
-let pe = document.createElement("p");
-let ul = document.createElement("ul");
-
-let namesito = document.getElementById('namesito');
-let skills = document.getElementById('skills');
-let portfolio = document.getElementById('portfolio');
+let codito = document.getElementsByTagName('code')
 
 class Objet {
   constructor(name, lastname) {
@@ -13,24 +8,19 @@ class Objet {
     this.skills = [];
     this.portfolio = [];
   }
-
   getFullName() {
     return `${this.name} ${this.lastname}`;
   }
-
   addSkills(String) {
     this.skills.push(String);
-  }
-  
+  }  
   addPortfolio(category, client) {
     let work = {
       category: category,
       client: client,
     };
-
     this.portfolio.push(work);
   }
-
   getClientsNames() {
     return this.portfolio.map((x) => {
       return x.client;
@@ -40,63 +30,24 @@ class Objet {
 
 // Create new objet
 let Tenco = new Objet ('Daniel', 'Tenconi');
-console.log(Tenco)
-
 // Add skills
 Tenco.addSkills('html 5');
 Tenco.addSkills('css 3');
 Tenco.addSkills('javaScript');
 Tenco.addSkills('jQuery');
 Tenco.addSkills('react JS');
-//console.log(Tenco.skills)
-//console.log(Tenco.countSkills())
-
+// Add works
 Tenco.addPortfolio('Graphic Design', 'Fogones Aiken');
 Tenco.addPortfolio('Web Design', 'NewAgroCoin');
 Tenco.addPortfolio('Design & Layout', 'Priceless Cities');
-//console.log(Tenco.getClientsNames())
 
+let namesito = document.createElement('div');
+namesito.innerHTML+=(`<em>name:</em> ${Tenco.name} <br><em>surname:</em> ${Tenco.lastname}`);
+let skillis = document.createElement('ul');
+let ski = Tenco.skills.forEach(e => skillis.innerHTML+=(`<li>${e}</li>`));
+let portis = document.createElement('ol');
+let port = Tenco.portfolio.forEach(w => portis.innerHTML+=(`<li>${w.client}<br><em>${w.category}</em></li>`));
 
-
-/* show.appendChild(
-  
-  namesito, skills) */
-  
-namesito.append (`${Tenco.name} ${Tenco.lastname}`)
-
-skills.append(`${Tenco.skills}`)
-let portis = Tenco.portfolio.map((po) =>{
-  return (`${po.client}  ${po.category}`)
-})
-portfolio.append(`${portis}`)
-
-
-
-/* 
-show.append(namesito, skills)
-
-namesito.appendChild(pe)
-pe.appendChild(
-  document.createTextNode(`Name & Lastname: ${Tenco.name} ${Tenco.lastname} `)
-)
-
-let ski = Tenco.skills.map((wo) =>{
-  return wo
-});
-
-skills.appendChild(pe)
-  pe.appendChild(
-    document.createTextNode(`Skills: ${ski}`)
-  ) */
-
-
-
-//show.innerHTML+=(`<strong>Skills</strong>: ${ski}`);
-
-/* let portis = Tenco.portfolio.map((po) =>{
-  return po
-}) */
-
-//show.innerHTML+=(`<strong>Portfolio</strong>: ${portis}`)
-
-//console.log(portis)
+show.appendChild(namesito)
+show.appendChild(skillis)
+show.appendChild(portis)
